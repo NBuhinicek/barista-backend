6d7d43479828d130ffba14ab0070bf1efa3586e4
Nikola Buhinicek <nikola.buhinicek@infinum.hr>
2019-02-03 21:09:23 +0100

    Add coordinates to model

---
+++ db/migrate/20190203155932_create_locations.rb
@@ -0,0 +1,13 @@
+class CreateLocations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :locations do |t|
+      t.string :street, null: false
+      t.integer :street_number, null: false
+      t.integer :zip_code, null: false
+      t.string :country, null: false
+      t.string :coordinates, null: false
+
+      t.timestamps
+    end
+  end
+end
