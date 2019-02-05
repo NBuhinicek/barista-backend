4429171f7f07267a6e78b52b73dbc6f899617c42
Nikola Buhinicek <nikola.buhinicek@infinum.hr>
2019-02-05 23:56:23 +0100

    Add qr code generator service

---
+++ app/services/qr_code_generator.rb
@@ -0,0 +1,15 @@
+require 'barby'
+require 'barby/barcode/qr_code'
+require 'barby/outputter/png_outputter'
+
+class QrCodeGenerator
+  def initialize(digest)
+    @digest = digest
+  end
+
+  def call
+    qr_code = Barby::QrCode.new(@digest, level: :q, size: 5)
+    base64 = Base64.encode64(qr_code.to_png(xdim: 5))
+    "data:image/png;base64,#{base64}"
+  end
+end
