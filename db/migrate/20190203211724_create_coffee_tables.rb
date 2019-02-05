938952d95e88df259bbf00467645a59a634ad040
Nikola Buhinicek <nikola.buhinicek@infinum.hr>
2019-02-05 22:10:49 +0100

    Add number of table integer

--- db/migrate/20190203211724_create_coffee_tables.rb
+++ db/migrate/20190203211724_create_coffee_tables.rb
@@ -2,6 +2,7 @@ class CreateCoffeeTables < ActiveRecord::Migration[5.2]
   def change
     create_table :coffee_tables do |t|
       t.belongs_to :coffee_shop
+      t.integer :table_number
       t.string :qr_code
       t.string :qr_code_hash
 
