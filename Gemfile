f8150310d9f5546904932c9603a1045315c9508d
Nikola Buhinicek <nikola.buhinicek@infinum.hr>
2019-02-06 21:51:26 +0100

    Add slim templates

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'puma', '~> 3.11'
 gem 'rails', '~> 5.2.2'
 gem 'rqrcode'
 gem 'sass-rails', '~> 5.0'
+gem 'slim'
 gem 'turbolinks', '~> 5'
 gem 'uglifier', '>= 1.3.0'
 
--- app/controllers/coffee_tables_controller.rb
+++ app/controllers/coffee_tables_controller.rb
@@ -2,4 +2,8 @@ class CoffeeTablesController < ApplicationController
   def index
     @coffee_tables = CoffeeTable.all
   end
+
+  def show
+    @coffee_table = CoffeeTable.find(params[:id])
+  end
 end
