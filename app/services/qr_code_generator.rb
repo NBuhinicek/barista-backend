f1c8cc1d3a83830cfc9d0011be3ac7c17816499d
Nikola Buhinicek <nikola.buhinicek@infinum.hr>
2019-02-06 14:35:23 +0100

    Set bigger qr code dimension

--- app/services/qr_code_generator.rb
+++ app/services/qr_code_generator.rb
@@ -8,8 +8,8 @@ class QrCodeGenerator
   end
 
   def call
-    qr_code = Barby::QrCode.new(@digest, level: :q, size: 5)
-    base64 = Base64.encode64(qr_code.to_png(xdim: 5))
+    qr_code = Barby::QrCode.new(@digest, level: :q, size: 6)
+    base64 = Base64.encode64(qr_code.to_png(xdim: 6))
     "data:image/png;base64,#{base64}"
   end
 end
