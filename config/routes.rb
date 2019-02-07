64a8d9c43ed8800e7a03c44b7f1fd604a6e338a4
Nikola Buhinicek <nikola.buhinicek@infinum.hr>
2019-02-07 20:46:35 +0100

    Add colors to template

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   resources :coffee_shops
   resources :coffee_tables
   devise_for :users
+
+  get '/qr_code/:hash', to: 'coffee_tables#qr_code'
+
+  root to: 'coffee_shops#index'
 end
