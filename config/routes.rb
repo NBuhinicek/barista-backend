7c659fcd37b893a71b7e033384d56e6ad42ca602
Nikola Buhinicek <nikola.buhinicek@infinum.hr>
2019-02-16 13:55:53 +0100

    Devise confirmation text

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,5 @@ Rails.application.routes.draw do
 
   resources :coffee_shops
   resources :coffee_tables
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
 end
