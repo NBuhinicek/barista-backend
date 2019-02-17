8884fa5c3576cc87652b1bde213ed6aff61ac12b
Nikola Buhinicek <nikola.buhinicek@infinum.hr>
2019-02-17 14:02:52 +0100

    WIP

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class LocationsController < ApiController
+      skip_before_action :authenticate
+
       def index
         render json: LocationSerializer.new(Location.all, include: [:coffee_shop]).serialized_json
       end
